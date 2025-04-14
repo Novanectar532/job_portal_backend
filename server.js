ca70c29ee3a501099c3e96ea5f5e5a6ccb559dc5
Santosh Kumar <sk8065788@gmail.com>
2025-04-14 16:31:14 +0530

    fix the cor for website

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Updated CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'https://job-portal-0dok.onrender.com', // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
